1155fff30b318ba82497fa73cb41c2bd4b1aee53
unknown <muneebali500@gmail.com>
2021-04-23 14:54:01 +0500

    8.user/3.update

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -14,3 +14,48 @@ export const read = (req, res) => {
     res.json(user);
   });
 };
+
+export const update = (req, res) => {
+  // console.log(`Update user - req.user`, req.user, `Update data`, req.body);
+
+  const { name, password } = req.body;
+
+  User.findOne({ _id: req.user._id }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: `User not found`,
+      });
+    }
+
+    if (!name) {
+      return res.status(400).json({
+        error: `Name is required`,
+      });
+    } else {
+      user.name = name;
+    }
+
+    if (password) {
+      if (password.length < 6) {
+        return res.status(400).json({
+          error: `Password should be minimum 6 characters long`,
+        });
+      } else {
+        user.password = password;
+      }
+    }
+
+    user.save((err, updatedUser) => {
+      if (err) {
+        console.log(`Update error`, err);
+        return res.status(400).json({
+          error: `Update failed`,
+        });
+      }
+
+      updatedUser.hashed_password = undefined;
+      updatedUser.salt = undefined;
+      res.json(updatedUser);
+    });
+  });
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 // import controllers
 import { requireSignin } from "../controllers/auth.js";
-import { read } from "../controllers/user.js";
+import { read, update } from "../controllers/user.js";
 
 router.get(`/user/:id`, requireSignin, read);
+router.put(`/user/update`, requireSignin, update);
 
 export default router;
