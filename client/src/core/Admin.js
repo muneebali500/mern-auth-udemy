cbf565e8a0c80af49a8514c21fa994a578b0d3dd
unknown <muneebali500@gmail.com>
2021-04-23 17:20:18 +0500

    8.user/9.admin

--- client/src/core/Admin.js
+++ client/src/core/Admin.js
@@ -1,9 +1,142 @@
-import Layout from "./Layout";
+import { useState, useEffect } from "react";
+import { Link, Redirect } from "react-router-dom";
+import Layout from "../core/Layout";
+import axios from "axios";
+import { isAuth, getCookie, signout, updateUser } from "../auth/helpers";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
-export default function Admin() {
+export default function Admin({ history }) {
+  const [values, setValues] = useState({
+    role: "",
+    name: "",
+    email: "",
+    password: "",
+    buttonText: "Submit",
+  });
+
+  const token = getCookie(`token`);
+
+  useEffect(() => {
+    loadProfile();
+  }, []);
+
+  const loadProfile = () => {
+    axios({
+      method: `GET`,
+      url: `${process.env.REACT_APP_API}/user/${isAuth()._id}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        // console.log(`Private Profile Update`, response);
+        const { role, name, email } = response.data;
+        setValues({ ...values, role, name, email });
+      })
+      .catch((error) => {
+        // console.log(`Private profile update error`, error.response.data.error);
+        if (error.response.status === 401) {
+          signout(() => {
+            history.push(`/`);
+          });
+        }
+      });
+  };
+
+  const { role, name, email, password, buttonText } = values;
+
+  const handleChange = (name) => (event) => {
+    // console.log(event.target.value);
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, buttonText: "Submitting" });
+    axios({
+      method: "PUT",
+      url: `${process.env.REACT_APP_API}/admin/update`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: { name, email, password },
+    })
+      .then((response) => {
+        console.log("Profile Update SUCCESS", response);
+        updateUser(response, () => {
+          setValues({
+            ...values,
+            buttonText: "Submitted",
+          });
+          toast.success(`Profile has been updated successfully`);
+        });
+      })
+      .catch((error) => {
+        // console.log("Profile Update ERROR", error.response.data);
+        setValues({ ...values });
+        toast.error(error.response.data.error);
+      });
+  };
+
+  const adminForm = () => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Role</label>
+        <input
+          defaultValue={role}
+          type="text"
+          className="form-control"
+          disabled
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          onChange={handleChange("name")}
+          value={name}
+          type="text"
+          className="form-control"
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          defaultValue={email}
+          type="email"
+          className="form-control"
+          disabled
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          onChange={handleChange("password")}
+          value={password}
+          type="password"
+          className="form-control"
+        />
+      </div>
+
+      <div>
+        <button className="btn btn-primary" onClick={clickSubmit}>
+          {buttonText}
+        </button>
+      </div>
+    </form>
+  );
+
   return (
     <Layout>
-      <h1>Admin Page</h1>
+      <div className="col-md-6 offset-md-3">
+        <ToastContainer />
+        <h1 className="pt-5 text-center">Admin</h1>
+        <p className="lead text-center">Admin Update</p>
+        {adminForm()}
+      </div>
     </Layout>
   );
 }
